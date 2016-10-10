74de9ca224a201308f57dec5d63444579c1b739c
Jeff Bumgardner <bumgardner.jeffrey@gmail.com>
2016-10-10 13:18:43 -0400

    madlib set up. waiting for partner

---
+++ madlib.js
@@ -0,0 +1,45 @@
+//variable to input into mad lib scheme//
+var verb1 = ''
+var verb2 = ''
+var noun1 = ''
+var nounOrName = ''
+var phrase1 = ''
+var adjective1 = ''
+var verb3 = ''
+var noun2 = ''
+var noun3 = ''
+var phrase2 = ''
+var noun3 = ''
+var noun4 = ''
+var verb4 = ''
+var noun5 = ''
+var nounOrName2 = ''
+var phrase2 = ''
+var famousGroup = ''
+var actionPhrase1 = ''
+var actionPhrase2 = ''
+var famousPerson = ''
+//mad lib story//
+
+var sentence = `1. Don't change horses until they......${verb1}.
+2. Strike while the bug is........................${verb2}.
+3. It's always darkest before...................${noun1} .
+4. Never underestimate the power of..... ${nounOrName}.
+5. You can lead a horse to water but....... ${phrase1}?
+6. Don't bite the hand that looks.............. ${adjective1}.
+7. No news is.................................. ${verb3}.
+8. A miss is as good as a......................... ${noun2}.
+9. You can't teach an old dog new.......... ${noun3}.
+10. If you lie down with dogs, you'll........ ${phrase2}.
+11. Love all, trust.......................... ${noun3}.
+12. The pen is mightier than the............. ${noun4}.
+13. An idle mind is the best way to.......... ${verb4}.
+14. Where there's smoke there's............ ${noun5}.
+15. Happy the bride who gets all the.......... ${nounOrName2}.
+16. A penny saved is............................... ${phrase2}.
+17. Two's company, three's..................... ${famousGroup}
+18. Don't put off til tomorrow what you put on... ${actionPhrase1}.
+19. Laugh and the whole world laughs
+        with you, cry and............. you ${actionPhrase2}.
+20. There are none so blind as................ ${famousPerson}.`
+console.log(sentence);
