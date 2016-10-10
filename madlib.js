42646d1abfb026e316e2efb50f8d5faf2b7e75c2
Jeff Bumgardner <bumgardner.jeffrey@gmail.com>
2016-10-10 13:37:43 -0400

    partner inserted answers

--- madlib.js
+++ madlib.js
@@ -1,29 +1,29 @@
 //variable to input into mad lib scheme//
-var verb1 = ''
-var verb2 = ''
-var noun1 = ''
-var nounOrName = ''
-var phrase1 = ''
-var adjective1 = ''
-var verb3 = ''
-var noun2 = ''
-var noun3 = ''
-var phrase2 = ''
-var noun3 = ''
-var noun4 = ''
-var verb4 = ''
-var noun5 = ''
-var nounOrName2 = ''
-var phrase2 = ''
-var famousGroup = ''
-var actionPhrase1 = ''
-var actionPhrase2 = ''
-var famousPerson = ''
+var verb1 = 'falling'
+var verb2 = 'tasteing'
+var noun1 = 'fireman'
+var nounOrName = 'Budha'
+var phrase1 = 'risk is our business'
+var adjective1 = 'whimpering'
+var verb3 = 'blowing'
+var noun2 = 'software'
+var noun3 = 'bloatware'
+var phrase2 = 'hard stop on the cocaine'
+var noun0 = 'K9 Unit'
+var noun4 = 'bike'
+var verb4 = 'floating'
+var noun5 = 'building'
+var nounOrName2 = 'Drumpf'
+var phrase3 = 'a little more to the left'
+var famousGroup = 'Dingo Ate My Baby'
+var actionPhrase1 = 'to the Batmobile!'
+var actionPhrase2 = 'ouch my liver!'
+var famousPerson = 'Molly Ringwald'
 //mad lib story//
 
-var sentence = `1. Don't change horses until they......${verb1}.
+var sentence = `1. Don't change horses until they are..${verb1}.
 2. Strike while the bug is........................${verb2}.
-3. It's always darkest before...................${noun1} .
+3. It's always darkest before the.................${noun1} .
 4. Never underestimate the power of..... ${nounOrName}.
 5. You can lead a horse to water but....... ${phrase1}?
 6. Don't bite the hand that looks.............. ${adjective1}.
@@ -31,12 +31,12 @@ var sentence = `1. Don't change horses until they......${verb1}.
 8. A miss is as good as a......................... ${noun2}.
 9. You can't teach an old dog new.......... ${noun3}.
 10. If you lie down with dogs, you'll........ ${phrase2}.
-11. Love all, trust.......................... ${noun3}.
+11. Love all, trust.......................... ${noun0}.
 12. The pen is mightier than the............. ${noun4}.
 13. An idle mind is the best way to.......... ${verb4}.
 14. Where there's smoke there's............ ${noun5}.
 15. Happy the bride who gets all the.......... ${nounOrName2}.
-16. A penny saved is............................... ${phrase2}.
+16. A penny saved is............................... ${phrase3}.
 17. Two's company, three's..................... ${famousGroup}
 18. Don't put off til tomorrow what you put on... ${actionPhrase1}.
 19. Laugh and the whole world laughs
